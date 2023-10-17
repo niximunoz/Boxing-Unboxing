72f2a78f02d7f3079bb16e00100b40e0486af60d
niximunoz <niximunozr@gmail.com>
2023-10-17 11:41:59 -0300

    boxing/unboxing

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿List<object> ListaObjetos = new List<object>();
+
+// Agregar los valores a la lista
+ListaObjetos.Add(7);
+ListaObjetos.Add(28);
+ListaObjetos.Add(-1);
+ListaObjetos.Add(true);
+ListaObjetos.Add("chair");
+
+// Imprimir todos los valores en la lista
+Console.WriteLine("Valores en lista:");
+foreach (object valores in ListaObjetos)
+{
+    Console.WriteLine(valores);
+}
+Console.WriteLine("-----------");
+for(int i = 0; i< ListaObjetos.Count; i++){
+    Console.WriteLine(ListaObjetos[i]);
+}
+
+// Variable para almacenar la suma de los valores enteros
+int sum = 0;
+
+// Recorrer lista y sumar valores enteros
+foreach (object valores in ListaObjetos)
+{
+    if (valores is int)
+    {
+        sum += (int)valores;
+    }
+}
+Console.WriteLine("-----------");
+// Suma de los valores enteros
+Console.WriteLine("Suma de los valores enteros: " + sum);
---
+++ README.md
@@ -0,0 +1,8 @@
+### Boxing/Unboxing
+
+- Create an empty List of type object.
+- Add the following values to the list: 7, 28, -1, true, "chair".
+- Loop through the list and print all values.
+- Add all values that are Int type together and output the sum.
+
+> Nixi Muñoz R.
